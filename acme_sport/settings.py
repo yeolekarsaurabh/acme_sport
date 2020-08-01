4c0b601cc45be8fb79dc46bc03e9b7a5e2e94b34
Saurabh Yeolekar <saurabh.yeolekar@smu.ca>
2020-08-01 16:19:28 -0300

    Added run config

--- acme_sport/settings.py
+++ acme_sport/settings.py
@@ -1,9 +1,7 @@
 import os
 from os.path import join, dirname
-from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 SCOREBOARD_URL='https://delivery.chalk247.com/scoreboard'
